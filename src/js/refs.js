a3474461caa7a20d091c1762b8670963f8a560a4
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-10-20 18:52:24 +0300

    Create refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+export const refs = {
+    searchForm: document.querySelector('#search-form'),
+    gallery: document.querySelector('.gallery'),
+    loadMore: document.querySelector('.load-more'),
+    target: document.querySelector('.gallery__link:last-child'),
+
+}
+
